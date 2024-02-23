da7979688c09dbd7989acdeeb36c458ff0a98df1
Romeo Franco Barro <romeofrancobarro@gmail.com>
2024-02-23 15:19:32 +0800

    Changed the layout for smaller phones

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButton from './components/TopButton';
@@ -42,18 +41,11 @@ function App() {
     };
   }, []);
 
-  
-
-
-
   return (
-    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <div className="mx-auto max-w-screen-lg lg:mt-4 py-5 px-4 md:px-8 lg:px-16 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       {/*<TopButton/>*/}
-      
-
       {weatherData && (
         <div>
-          
           <TimeAndLocation latitude={weatherData.latitude} longitude={weatherData.longitude} />
           <TemperatureAndDetails temperature={weatherData.current.temperature2m} 
                                   humidity={weatherData.current.relativeHumidity2m} 
@@ -64,16 +56,13 @@ function App() {
                                   high={weatherData.daily.apparentTemperatureMax[1]} 
                                   low={weatherData.daily.apparentTemperatureMin[1]}/> 
 
-          <div className="flex">
-                <div className="w-1/2">
-                  <WaterLevelTable /> {/* Render WaterLevelTable on the left side */}
-                </div>
-                <div className="w-1/2 flex flex-col">
-                  <div className="flex-grow">
-                  <Forecast title="hourly forecast" data={weatherData.hourly} hourly={weatherData.hourly} hourlyT={weatherData.hourly.time}/> {/* Render hourly forecast on the top right */}
-                  </div>
-                  <div className="flex-grow">
-                  <Forecast title="daily forecast" data={weatherData.daily} 
+          <div className="flex flex-col md:flex-row">
+            <div className="md:w-1/2 md:pr-2">
+              <WaterLevelTable /> {/* Render WaterLevelTable on the left side */}
+            </div>
+            <div className="md:w-1/2 md:pl-2">
+              <Forecast title="hourly forecast" data={weatherData.hourly} hourly={weatherData.hourly} hourlyT={weatherData.hourly.time}/> {/* Render hourly forecast on the top right */}
+              <Forecast title="daily forecast" data={weatherData.daily} 
                                             dt1={weatherData.daily.temperature2mMax[2]}
                                             dt2={weatherData.daily.temperature2mMax[3]}
                                             dt3={weatherData.daily.temperature2mMax[4]}
@@ -84,15 +73,8 @@ function App() {
                                             dc3={weatherData.daily.weatherCode[4]}
                                             dc4={weatherData.daily.weatherCode[5]}
                                             dc5={weatherData.daily.weatherCode[6]}  />{/* Render daily forecast on the bottom right */}
-                  </div>
-                </div>
-              </div>
-
-          
-
-         
-                                            
-      
+            </div>
+          </div>
         </div>
       )}
     </div>
@@ -100,4 +82,3 @@ function App() {
 }
 
 export default App;
-
